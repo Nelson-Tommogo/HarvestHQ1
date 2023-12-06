0773dc61fde85ef76093da07db906245cafe619e
Nelson-Tommogo <nelsontommogo9@gmail.com>
2023-12-06 13:40:08 +0300

    HarvestHQ set Up

--- app/src/main/java/com/example/harvesthq/components/appcomponents.kt
+++ app/src/main/java/com/example/harvesthq/components/appcomponents.kt
@@ -1,7 +1,27 @@
 package com.example.harvesthq.components
 
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import kotlin.math.min
 
 @Composable
-fun appcomponents() {
+fun NormalTextComponent(value: String) {
+    Text(
+        text = value,
+        modifier = Modifier.fillMaxWidth().height(80.dp),
+        style = TextStyle(
+            fontSize = 16.sp,
+            fontWeight = FontWeight.Normal,
+            fontStyle = FontStyle.Normal,
+        )
+    )
 }
