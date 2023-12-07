687ef5a97030cc82d84b85502d1e62eba8ae3ec9
Nelson-Tommogo <nelsontommogo9@gmail.com>
2023-12-07 22:11:16 +0300

    Bottom Navigation

--- app/src/main/java/com/example/harvesthq/components/Home.kt
+++ app/src/main/java/com/example/harvesthq/components/Home.kt
@@ -1,33 +1,77 @@
-package com.example.harvesthq.components
-
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.material3.Text
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material.icons.filled.Send
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material3.*
+import androidx.compose.material3.icons.Icons
+import androidx.compose.material3.icons.filled.Home
+import androidx.compose.material3.icons.filled.MailOutline
+import androidx.compose.material3.icons.filled.PersonOutline
+import androidx.compose.material3.icons.filled.Search
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontStyle
-import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
+import com.example.harvesthq.R
+
 
 @Composable
-fun NormalTextComponent(value: String) {
-    Text(
-        text = value,
-        modifier = Modifier.fillMaxWidth().height(80.dp),
-        style = TextStyle(
-            fontSize = 16.sp,
-            fontWeight = FontWeight.Normal,
-            fontStyle = FontStyle.Normal,
+fun Bottomnav(value: String) {
+    Column(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        // Your existing content
+        Text(
+            text = value,
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(80.dp),
+            style = MaterialTheme.typography.bodySmall // Use a text style from MaterialTheme
         )
-    )
-}
 
+        // Bottom Navigation
+        BottomNavigation(
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(MaterialTheme.colorScheme.primary)
+        ) {
+            // Bottom Navigation Items
+            BottomNavigationItem(
+                icon = { Icon(Icons.Default.Home, contentDescription = null) },
+                label = { Text(text = stringResource(id = R.string.Home)) },
+                selected = false, // Set selected to true for the active tab
+                onClick = { /* Handle navigation to Home */ }
+            )
+            BottomNavigationItem(
+                icon = { Icon(Icons.Default.Search, contentDescription = null) },
+                label = { Text(text = stringResource(id = R.string.articles)) },
+                selected = false,
+                onClick = { /* Handle navigation to articles */ }
+            )
+            BottomNavigationItem(
+                icon = { Icon(Icons.Default.Send, contentDescription = null) },
+                label = { Text(text = stringResource(id = R.string.community)) },
+                selected = false,
+                onClick = { /* Handle navigation to community */ }
+            )
+            BottomNavigationItem(
+                icon = { Icon(Icons.Default.Settings, contentDescription = null) },
+                label = { Text(text = stringResource(id = R.string.profile)) },
+                selected = false,
+                onClick = { /* Handle navigation to Profile */ }
+            )
+        }
+    }
+}
 
 @Preview
 @Composable
-fun DefaultPreviewOfSignUpScreen() {
-    NormalTextComponent(value = "Sample Text for Preview")
+fun DefaultPreviewOfBottomNavigation() {
+    Surface {
+        Bottomnav(value = "Sample Text for Preview")
+    }
 }
