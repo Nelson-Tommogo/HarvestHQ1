c07b295b216621f9d793630606298ca8f683f6a0
Nelson-Tommogo <nelsontommogo9@gmail.com>
2023-12-13 14:58:35 +0300

    bottom nav

--- app/src/main/java/com/example/harvesthq/components/bottomnav.kt
+++ app/src/main/java/com/example/harvesthq/components/bottomnav.kt
@@ -1,9 +1,10 @@
 package com.example.harvesthq.components
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Email
@@ -15,6 +16,7 @@ import androidx.compose.material.icons.outlined.Home
 import androidx.compose.material.icons.outlined.Search
 import androidx.compose.material.icons.outlined.Settings
 import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
@@ -22,21 +24,21 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.compose.ui.res.colorResource
 import androidx.navigation.NavHostController
-import com.example.harvesthq.R
 
 data class BottomNavigationItem(
     val title: String,
     val selectedIcon: ImageVector,
     val unselectedIcon: ImageVector,
     val hasNews: Boolean,
-    val white: Color = colorResource(id = R.color.white1),
-    val badgeCount: Int? = null
+    val badgeCount: Int? = null,
+    val color: Color = Color.Unspecified,
+    val contentColor: Color = Color.Unspecified
 )
 
 @OptIn(ExperimentalMaterial3Api::class)
-class bottomnav(navController: NavHostController) : ComponentActivity() {
+class bottomnav(private val navController: NavHostController) : ComponentActivity() {
+    @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -46,26 +48,28 @@ class bottomnav(navController: NavHostController) : ComponentActivity() {
                         title = "Home",
                         selectedIcon = Icons.Filled.Home,
                         unselectedIcon = Icons.Outlined.Home,
-                        hasNews = false,
+                        hasNews = false
                     ),
                     BottomNavigationItem(
                         title = "Community",
                         selectedIcon = Icons.Filled.Search,
                         unselectedIcon = Icons.Outlined.Search,
                         hasNews = false,
-                        badgeCount = 45
+                        badgeCount = 45,
+                        color = Color.Red,
+                        contentColor = Color.Black
                     ),
                     BottomNavigationItem(
                         title = "Articles",
                         selectedIcon = Icons.Filled.Email,
                         unselectedIcon = Icons.Outlined.Email,
-                        hasNews = true,
+                        hasNews = true
                     ),
                     BottomNavigationItem(
                         title = "Settings",
                         selectedIcon = Icons.Filled.Settings,
                         unselectedIcon = Icons.Outlined.Settings,
-                        hasNews = true,
+                        hasNews = true
                     ),
                 )
                 var selectedItemIndex by rememberSaveable {
@@ -73,11 +77,14 @@ class bottomnav(navController: NavHostController) : ComponentActivity() {
                 }
                 Surface(
                     modifier = Modifier.fillMaxSize(),
-                    color = colorResource(id = R.color.white)
+                    color = Color.Red // Set the background color to red
                 ) {
                     Scaffold(
                         bottomBar = {
-                            NavigationBar {
+                            NavigationBar(
+                                modifier = Modifier.background(Color.Red), // Set the background color to red
+                                contentColor = Color.Black // Set the text color to black
+                            ) {
                                 items.forEachIndexed { index, item ->
                                     NavigationBarItem(
                                         selected = selectedItemIndex == index,
@@ -86,17 +93,20 @@ class bottomnav(navController: NavHostController) : ComponentActivity() {
                                             // navController.navigate(item.title)
                                         },
                                         label = {
-                                            Text(text = item.title)
+                                            Text(
+                                                text = item.title,
+                                                color = if (item.color != Color.Unspecified) item.color else Color.Black
+                                            )
                                         },
                                         alwaysShowLabel = false,
                                         icon = {
                                             BadgedBox(
                                                 badge = {
-                                                    if(item.badgeCount != null) {
+                                                    if (item.badgeCount != null) {
                                                         Badge {
                                                             Text(text = item.badgeCount.toString())
                                                         }
-                                                    } else if(item.hasNews) {
+                                                    } else if (item.hasNews) {
                                                         Badge()
                                                     }
                                                 }
@@ -112,15 +122,18 @@ class bottomnav(navController: NavHostController) : ComponentActivity() {
                                     )
                                 }
                             }
-                        }, content = function()
+                        },
+                        content = {
+                            Function()
+                        }
                     )
                 }
             }
         }
     }
 
-    private fun function(): (PaddingValues) -> Unit = {
-
+    @Composable
+    private fun Function() {
+        // Your content goes here
     }
 }
-
