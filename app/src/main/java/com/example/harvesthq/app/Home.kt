ea1dfe8aa5f25e24158f37a652bf497146ace96f
Nelson-Tommogo <nelsontommogo9@gmail.com>
2023-12-19 17:12:44 +0300

    Home

---
+++ app/src/main/java/com/example/harvesthq/app/Home.kt
@@ -0,0 +1,112 @@
+package com.example.harvesthq.app
+
+import android.annotation.SuppressLint
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.navigation.compose.rememberNavController
+import com.example.harvesthq.R
+
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@Composable
+fun HomePage() {
+    Scaffold(
+        topBar = { AppBar() },
+        content = { Content() }
+    )
+}
+
+@Composable
+fun AppBar() {
+    // Container for your app bar with logo, search, and settings
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(16.dp),
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+
+        Image(
+            painter = painterResource(id = R.drawable.harvesthqlogohrs), // Placeholder logo
+            contentDescription = "Harvest Logo",
+            modifier = Modifier.size(48.dp)
+        )
+
+
+        Icon(
+            imageVector = Icons.Default.Search,
+            contentDescription = "Search",
+            modifier = Modifier.clickable {
+
+            }
+        )
+
+
+        Icon(
+            imageVector = Icons.Default.Settings,
+            contentDescription = "Settings",
+            modifier = Modifier.clickable {
+                // Handle settings click
+            }
+        )
+    }
+}
+
+@Composable
+fun Content() {
+
+    ContainerForAgriculturalProducts()
+
+
+    ContainerForFrequentlyOrderedProducts()
+}
+
+@Composable
+fun ContainerForAgriculturalProducts() {
+
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(16.dp)
+    ) {
+        Text("HarvestHQ Products")
+    }
+}
+
+@Composable
+fun ContainerForFrequentlyOrderedProducts() {
+
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(16.dp)
+    ) {
+        Text("Frequently Ordered Products")
+
+    }
+}
+
+@Preview
+@Composable
+fun HomePagePreview() {
+    val navController = rememberNavController()
+    HomePage()
+}
