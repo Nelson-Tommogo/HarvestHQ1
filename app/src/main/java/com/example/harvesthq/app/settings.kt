5f55aced957e33454421752d77207f28883f5650
Nelson-Tommogo <nelsontommogo9@gmail.com>
2023-12-26 10:32:52 +0300

    HarvestHq

--- app/src/main/java/com/example/harvesthq/app/settings.kt
+++ app/src/main/java/com/example/harvesthq/app/settings.kt
@@ -1,4 +1,27 @@
 package com.example.harvesthq.app
 
-class settings {
+import android.annotation.SuppressLint
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.harvesthq.navigation.Topbar
+
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@Composable
+fun settings() {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        content = {
+            Topbar()
+        }
+    )
+}
+
+
+@Preview
+@Composable
+fun settingPreview(){
+    settings()
 }
