a2462c197d0b0439323f6817139e03a85f1a97e9
Nelson-Tommogo <nelsontommogo9@gmail.com>
2023-12-26 10:34:40 +0300

    HarvestHq

--- app/src/main/java/com/example/harvesthq/app/community.kt
+++ app/src/main/java/com/example/harvesthq/app/community.kt
@@ -1,4 +1,28 @@
 package com.example.harvesthq.app
 
-class community {
+import android.annotation.SuppressLint
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.harvesthq.navigation.Topbar
+
+
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@Composable
+fun community() {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        content = {
+            Topbar()
+        }
+    )
+}
+
+
+@Preview
+@Composable
+fun communityPreview(){
+    settings()
 }
