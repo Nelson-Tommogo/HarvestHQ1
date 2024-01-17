f4dbac9b195f33bffdbc4a09493351bfe0ef2770
Nelson-Tommogo <nelsontommogo9@gmail.com>
2024-01-17 18:48:12 +0300

    index

--- app/src/main/java/com/example/harvesthq/app/Home.kt
+++ app/src/main/java/com/example/harvesthq/app/Home.kt
@@ -1,7 +1,6 @@
 package com.example.harvesthq.app
 
 import android.annotation.SuppressLint
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -10,9 +9,6 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.Card
 import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -21,7 +17,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.colorResource
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
@@ -55,57 +50,6 @@ fun Adds(){
 fun ADDS(){
 }
 
-@Composable
-@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
-fun AppBar() {
-    val items = listOf(
-        Icons.Default.Search to "Search"
-    )
-
-    Row(
-        modifier = Modifier
-            .fillMaxWidth()
-            .height(70.dp)
-            .background(color = colorResource(id = R.color.homecards))
-            .padding(16.dp),
-        horizontalArrangement = Arrangement.SpaceBetween,
-        verticalAlignment = Alignment.CenterVertically
-    ) {
-
-        Image(
-            painter = painterResource(id = R.drawable.harvesthqlogo),
-            contentDescription = "Harvest Logo",
-            modifier = Modifier.size(150.dp)
-        )
-//        Image(
-//            painter = painterResource(id = R.drawable.search),
-//            contentDescription = "Search",
-//            modifier = Modifier
-//                .clickable {
-//                }
-//                .size(26.dp)
-//        )
-
-//        Image(
-//            painter = painterResource(id = R.drawable.scanqr),
-//            contentDescription = "scan",
-//            modifier = Modifier
-//                .clickable {
-//
-//                }
-//                .size(26.dp)
-//        )
-//        Image(
-//            painter = painterResource(id = R.drawable.set),
-//            contentDescription = "App setting",
-//            modifier = Modifier
-//                .clickable {
-//
-//                }
-//                .size(26.dp)
-//        )
-    }
-}
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
