7b69f5d3c8d22053c34150fbafacef88f79806d3
Nelson-Tommogo <nelsontommogo9@gmail.com>
2024-02-13 09:47:08 +0300

    more here

--- app/src/main/java/com/example/harvesthq/navigation/topbar.kt
+++ app/src/main/java/com/example/harvesthq/navigation/topbar.kt
@@ -3,15 +3,16 @@ import android.annotation.SuppressLint
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Email
 import androidx.compose.material.icons.filled.List
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.Card
@@ -29,6 +30,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.input.ImeAction
@@ -50,29 +52,33 @@ fun Topbar() {
     Row(
         modifier = Modifier
             .fillMaxWidth()
-            .height(70.dp)
+            .height(100.dp)
             .background(color = colorResource(id = R.color.homecards))
             .padding(16.dp),
         horizontalArrangement = Arrangement.SpaceBetween,
         verticalAlignment = Alignment.CenterVertically
     ) {
-        Icon(
-            painter = painterResource(id = R.drawable.harvesthqlogo),
-            contentDescription = "Harvest Logo",
-            modifier = Modifier.size(120.dp)
-        )
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(color = Color.Gray)
+        ) {
+            Icon(
+                painter = painterResource(id = R.drawable.harvesthqlogo),
+                contentDescription = "Harvest Logo",
+                modifier = Modifier
+                    .size(150.dp)
+                    .padding(16.dp)
+            )
+        }
+
 
         OutlinedTextField(
             value = searchText,
             onValueChange = { searchText = it },
             label = {
                 Row {
-                    Icon(
-                        imageVector = Icons.Default.Email,
-                        contentDescription = null,
-                        tint = green1,
-                        modifier = Modifier.padding(end = 8.dp)
-                    )
+
                     Text("Search")
                 }
             },
@@ -111,6 +117,8 @@ fun Topbar() {
         )
     }
 }
+
+
 
 @Composable
 fun CardWithTopbar(content: @Composable () -> Unit) {
